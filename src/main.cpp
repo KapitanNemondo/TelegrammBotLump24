f8ed33579072f9f0b91f853e715a199adf1dc927
Dmitiy Elovskiy <dima20033467@yandex.ru>
2024-07-14 05:23:51 +0300

    Ускорена работа функций, добавлен визуальный интерфейс подключения к сети Wi-Fi

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,17 @@
 #include <FastLED.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>
-#include <string>
+#include <FastBot.h>
+//#include <string>
 #include <DHT.h>
 
-using namespace std; 
+//using namespace std; 
+
 #define WIFI_SSID       "Asus10"
-#define WIFI_PASSWORD   "pin34ok3"
+#define WIFI_PASS       "pin34ok3"
 #define BOT_TOKEN       "7351017309:AAFgWCeENL4lWavu56E4wYEWIAFYZPpmkkc"
+
+FastBot bot(BOT_TOKEN);
 
 // Порты
 #define DIR         5
@@ -25,7 +28,7 @@ TM1637Display display(CLK, DIR);
 
 unsigned long previousMillis = 0;
 unsigned long displayPreviousMillis = 0;
-const long interval = 2000;
+unsigned long rainMillis = 0;
 const long displayInterval = 1000;
 
 bool showTemperature = true;
@@ -35,12 +38,14 @@ bool autoBrightness = false;
 int bri = 0;
 int humidity = 0;
 int temp = 0;
-String mode = "static";   // Режим по умолчанию
-String color = "#ff0000";  // Цвет по умолчанию
 uint8_t current_bri = 0;
 
 void UpdateFoto();
 void UpdateTMP();
+void smoothBlinkYellow();
+void blinkGreenFourTimes();
+void newMsg(FB_msg& msg);
+void Raduga();
 
 DHT dht_14(DHTPIN, DHT11);
 
@@ -59,87 +64,15 @@ enum MODE {
   RADUGA_MODE
 } mood;
 
-enum DATA {
-  HUM,
-  TEMP,
-  BRIGHT
-};
-
 
 // ---------------------------------------------- Кодя для дисплея ----------------------------------------------
 
-const unsigned long BOT_MTBS = 100;
-X509List cert(TELEGRAM_CERTIFICATE_ROOT);
-WiFiClientSecure secured_client;
-UniversalTelegramBot bot(BOT_TOKEN, secured_client);
+const unsigned long BOT_MTBS = 50;
+
 unsigned long bot_lasttime;
 
 int ledStatus = 0;
-void handleNewMessages(int numNewMessages) {
-  for (int i = 0; i < numNewMessages; i++) {
-    String chat_id = bot.messages[i].chat_id;
-    String text = bot.messages[i].text;
-    String from_name = bot.messages[i].from_name;
-    // if (from_name == "")
-    // from_name = "Guest";
-    Serial.print("[Message text] "); Serial.println(text);
-    Serial.print("[Name text] "); Serial.println(from_name);
-    Serial.println();
 
-    if (text == "/gettemperature") {
-      bot.sendMessage(chat_id, (String("Температура: ") + String(temp) + String("℃")), "");
-    }
-    if (text == "/gethumidity") {
-      bot.sendMessage(chat_id, (String("Влажность: ") + String(humidity) + String("%")), "");
-    }
-    if (text == "/getbrightness") {
-      bot.sendMessage(chat_id, (String("Яркость: ") + String(bri) + String('%')), "");
-    }
-    if (text == "/setautobrightness") {
-      autoBrightness = !autoBrightness;
-      if (autoBrightness) {
-        bot.sendMessage(chat_id, "Включен режим автоматического изменения яркости", "");
-      } else {
-        bot.sendMessage(chat_id, "Активировано ручное управление яркостью", "");
-      }
-    }
-    if (text == "/setcolorwhite") {
-      bot.sendMessage(chat_id, "Цвет изменён на белый", "");
-      mode = "rgb";
-      selectedColor = CRGB::White; 
-    }
-    if (text == "/setcolorred") {
-      bot.sendMessage(chat_id, "Цвет изменён на красный", "");
-      mode = "rgb";
-      selectedColor = CRGB::Red;
-  }
-    if (text == "/setcolorgreen") {
-      bot.sendMessage(chat_id, "Цвет изменён на зелёный", "");
-      mode = "rgb";
-      selectedColor = CRGB::Green;
-    }
-    if (text == "/setcolorblue"){
-      bot.sendMessage(chat_id, "Цвет изменён на синий", "");
-      mode = "rgb";
-      selectedColor = CRGB::Blue;
-    }
-    if (text == "/setraindow") {
-      bot.sendMessage(chat_id, "Включен режим радуги", "");
-      mode = "rainbow";
-    }
-    if (text == "/upbrightness") {
-      current_bri += 10;
-      bot.sendMessage(chat_id, "Яркость увеличина на 10%\nТеущий уровень яркости: " + String(current_bri) + "%", "");
-      if (current_bri >= 100) current_bri = 0;
-    }
-    if (text == "/downbrightness") {
-      current_bri -= 10;
-      bot.sendMessage(chat_id, "Яркость уменьшена на 10%\nТеущий уровень яркости: " + String(current_bri) + "%", "");
-      if (current_bri <= 0) current_bri = 100;
-      
-    }
-  }
-}
 
 void setup() {
   Serial.begin(9600);
@@ -149,16 +82,26 @@ void setup() {
   
   
   // Инициализация ленты WS2812B
+  //FastLED.addLeds<LED_TYPE, LED_PIN_1, COLOR_ORDER>(leds, NUM_LEDS_1);
   FastLED.addLeds<LED_TYPE, LED_PIN_1, COLOR_ORDER>(leds, NUM_LEDS_1);
 
   
   Serial.println();
   configTime(0, 0, "pool.ntp.org");
-  secured_client.setTrustAnchors(&cert);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  // Serial.println("[Wi-Fi Name] " + String(WIFI_SSID));
+  // Serial.println("[Wi-Fi Pass] " + String(WIFI_PASS));
+
+  // delay(2000);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    smoothBlinkYellow();
   }
+
   Serial.print("WiFi connected. IP address: ");
   Serial.println(WiFi.localIP());
   Serial.print("Retrieving time: ");
@@ -166,7 +109,13 @@ void setup() {
   while (now < 24 * 3600) {
     delay(100);
     now = time(nullptr);
+    
   }
+
+  blinkGreenFourTimes();
+
+  bot.attach(newMsg);
+
 
   dht_14.begin();
 
@@ -174,93 +123,49 @@ void setup() {
 }
 
 void loop() {
-  if (millis() - bot_lasttime > BOT_MTBS) {
-    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    while (numNewMessages) {
-      handleNewMessages(numNewMessages);
-      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-    }
-  bot_lasttime = millis();
-  }
 
-
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= 10) {
-    previousMillis = currentMillis;
-    // Обновление данных с сенсоров
-    UpdateTMP();
-    UpdateFoto();
-  }
-
+  bot.tick();
+  // Обновление данных с сенсоров и дисплея
+  UpdateTMP();
+  UpdateFoto();
 }
+
 
 
 void UpdateFoto() {
   bri = map(analogRead(FOTO), 0, 1023, 0, 100);
-  
 
   if (autoBrightness) {
-    bri = map(bri, 0, 100, 255, 0);
     current_bri = bri;
   }
-  
 
   unsigned long currentMillis = millis();
 
-  if (mode == "static") {
-    mood = MODE::TEMP_SET;
-  } else if (mode == "rgb") {
-    mood = MODE::RGB_SET;
-  } else if (mode == "rainbow") {
-    mood = MODE::RADUGA_MODE;
-  }
-
-  switch (mood)
-  {
-  case MODE::TEMP_SET: {
-    for (int i = 0; i < NUM_LEDS_1; i++) {
-      // Преобразование температуры в цвет (пример, можно изменить по вкусу)
-      uint8_t red = map(temp, 15, 30, 0, 255);
-      uint8_t green = 0;
-      uint8_t blue = map(temp, 15, 30, 255, 0);
-      
-      leds[i] = CRGB(red, green, blue);
-
-      // Преобразование уровня освещенности в яркость
-      
-      leds[i].nscale8_video(bri);
+  switch (mood) {
+    case MODE::TEMP_SET: {
+      for (int i = 0; i < NUM_LEDS_1; i++) {
+        uint8_t red = map(temp, 15, 30, 0, 255);
+        uint8_t blue = map(temp, 15, 30, 255, 0);
+        leds[i] = CRGB(red, 0, blue);
+        leds[i].nscale8_video(bri);
+      }
+      break;
     }
-
-    // Обновление состояния светодиодов
-    FastLED.setBrightness(current_bri);
-    FastLED.show();
-    break;
-  }
-
-  case MODE::RGB_SET: {
-    // CRGB selectedColor = CRGB::Black;
-    // long number = strtol(&color[1], NULL, 16);  // Преобразование HEX в long
-    // selectedColor = CRGB((number >> 16) & 0xFF, (number >> 8) & 0xFF, number & 0xFF);
-    
-    for (int i = 0; i < NUM_LEDS_1; i++) {
-      leds[i] = selectedColor;
+    case MODE::RGB_SET: {
+      fill_solid(leds, NUM_LEDS_1, selectedColor);
+      break;
     }
-    FastLED.setBrightness(current_bri);
-    FastLED.show();
-    break;
+    case MODE::RADUGA_MODE: {
+      fill_rainbow(leds, NUM_LEDS_1, millis() / 10, 7);
+      //Raduga();
+      rainMillis = currentMillis;
+      break;
+    }
+    default:
+      break;
   }
-
-  case MODE::RADUGA_MODE: {
-    fill_rainbow(leds, NUM_LEDS_1, currentMillis / 10, 7);
-    FastLED.setBrightness(current_bri);
-    FastLED.show();
-    break;
-  }
-  default:
-    break;
-  }
-  
+  FastLED.setBrightness(map(current_bri, 0, 100, 0, 255));
+  FastLED.show();
 }
 
 void UpdateTMP() {
@@ -269,10 +174,10 @@ void UpdateTMP() {
   temp = dht_14.readTemperature();
 
   // Проверка на ошибки чтения
-  if (isnan(humidity) || isnan(temp)) {
-    Serial.println("Ошибка чтения с датчика DHT11!");
-    return;
-  } 
+  // if (isnan(humidity) || isnan(temp)) {
+  //   Serial.println("Ошибка чтения с датчика DHT11!");
+  //   return;
+  // } 
 
     // Обновление данных на дисплее TM1637
   if (currentMillis - displayPreviousMillis >= displayInterval) {
@@ -288,3 +193,119 @@ void UpdateTMP() {
     showTemperature = !showTemperature;  // Переключение между показом температуры и влажности
   }
 };
+
+
+// Функция плавного мигания желтого цвета
+void smoothBlinkYellow() {
+  static bool flag = true;
+  
+  if (flag) {
+    fill_solid(leds, NUM_LEDS_1, CRGB::Yellow);
+    FastLED.setBrightness(255);
+    FastLED.show();
+  } else {
+    fill_solid(leds, NUM_LEDS_1, CRGB::Black);
+    FastLED.show();
+  }
+
+  flag = !flag;
+}
+
+// Функция мигания зеленым цветом
+void blinkGreenFourTimes() {
+  for (int i = 0; i < 4; i++) {
+    fill_solid(leds, NUM_LEDS_1, CRGB::Green);
+    FastLED.setBrightness(255);
+    FastLED.show();
+    delay(250); // Задержка 250 мс для включенного состояния
+
+    fill_solid(leds, NUM_LEDS_1, CRGB::Black);
+    FastLED.show();
+    delay(250); // Задержка 250 мс для выключенного состояния
+  }
+}
+
+void Raduga() {
+  static byte counter = 0;
+  for (int i = 0; i < NUM_LEDS_1; i++) {
+    leds[i].setHue(counter + i * 255 / NUM_LEDS_1);
+  }
+  counter++;        // counter меняется от 0 до 255 (тип данных byte)
+}
+
+void newMsg(FB_msg& msg) {
+
+  if (msg.text == "/gettemperature") {
+    bot.sendMessage((String("Температура: ") + String(temp) + String("℃")), msg.chatID);
+
+  } else if (msg.text == "/gethumidity") {
+    bot.sendMessage((String("Влажность: ") + String(humidity) + String("%")), msg.chatID);
+
+  }else if (msg.text == "/getbrightness") {
+    bot.sendMessage((String("Яркость: ") + String(bri) + String('%')), msg.chatID);
+
+  } else if (msg.text == "/setautobrightness") {
+    autoBrightness = !autoBrightness;
+    if (autoBrightness) {
+      bot.sendMessage("Включен режим автоматического изменения яркости", msg.chatID);
+    } else {
+      bot.sendMessage("Активировано ручное управление яркостью", msg.chatID);
+    }
+  } else if (msg.text == "/setcolorwhite") {
+    bot.sendMessage("Цвет изменён на белый", msg.chatID);
+    mood = MODE::RGB_SET;
+    selectedColor = CRGB::White; 
+
+  } else if (msg.text == "/setcolorred") {
+    bot.sendMessage("Цвет изменён на красный", msg.chatID);
+    mood = MODE::RGB_SET;
+    selectedColor = CRGB::Red;
+
+  } else if (msg.text == "/setcolorgreen") {
+    bot.sendMessage("Цвет изменён на зелёный", msg.chatID);
+    mood = MODE::RGB_SET;
+    selectedColor = CRGB::Green;
+
+  } else if (msg.text == "/setcolorblue"){
+    bot.sendMessage("Цвет изменён на синий", msg.chatID);
+    mood = MODE::RGB_SET;
+    selectedColor = CRGB::Blue;
+
+  }else if (msg.text == "/setraindow") {
+    bot.sendMessage("Включен режим радуги", msg.chatID);
+    mood = MODE::RADUGA_MODE;
+
+  }else if (msg.text == "/upbrightness") {
+    current_bri = min(current_bri + 10, 100);  // Ограничение яркости до 100%
+    bot.sendMessage("Яркость увеличена на 10%\nТекущий уровень яркости: " + String(current_bri) + "%", msg.chatID);
+
+  }else if (msg.text == "/downbrightness") {
+    current_bri = max(current_bri - 10, 0);  // Ограничение яркости до 0%
+    bot.sendMessage("Яркость уменьшена на 10%\nТекущий уровень яркости: " + String(current_bri) + "%", msg.chatID);
+
+  } else {
+    bot.sendMessage("Введена не существующая команда", msg.chatID);
+  }
+  // if (text[0] == '#') {
+  //   //# 000 000 000
+  //   string customR = "";
+  //   string customG = "";
+  //   string customB = "";
+  //   for (int i = 2; i <= 4; i++)
+  //   {
+  //     customR += text[i];
+  //   }    
+  //   for (int i = 6; i <= 8; i++)
+  //   {
+  //     customG += text[i];
+  //   }   
+  //   for (int i = 10; i <= 12; i++)
+  //   {
+  //     customB += text[i];
+  //   }
+  //}
+  Serial.print("[Mode] "); Serial.println(mood);
+  Serial.print("[Current bri] "); Serial.println(current_bri);
+  Serial.print("[Bri data] "); Serial.println(bri);
+  Serial.println();
+}
